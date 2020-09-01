9d5bb25d6dc63690d079801899cf2d60e9e8a621
Blake Green <bgreen@intervision.com>
2020-09-01 16:55:26 -0700

    add pipeline resources

--- aws_cdk_python_pipelines/pipeline_stack.py
+++ aws_cdk_python_pipelines/pipeline_stack.py
@@ -1,6 +1,31 @@
 from aws_cdk import core
+from aws_cdk import aws_codepipeline as codepipeline
+from aws_cdk import aws_codepipeline_actions as cpactions
+from aws_cdk import pipelines
 
 class PipelineStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        source_artifact = codepipeline.Artifact()
+        cloud_assembly_artifact = codepipeline.Artifact()
+
+        pipelines.CdkPipeline(self, 'Pipeline',
+            cloud_assembly_artifact=cloud_assembly_artifact,
+            pipeline_name='CdkPipeline',
+            source_action=cpactions.GitHubSourceAction(
+                action_name='Github',
+                oauth_token= core.SecretValue.secrets_manager('github-token'),
+                output=source_artifact,
+                owner='bgreengo',
+                repo='aws-cdk-python-pipelines',
+                trigger=cpactions.GitHubTrigger.POLL
+            ),
+            synth_action=pipelines.SimpleSynthAction(
+                source_artifact=source_artifact,
+                cloud_assembly_artifact=cloud_assembly_artifact,
+                install_command='npm install -g aws-cdk && pip install -r requirements.txt',
+                synth_command='cdk synth'
+            )
+        )
